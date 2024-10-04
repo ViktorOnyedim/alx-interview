e37ee90be4930a57300237e92d5267011ce26894
ViktorOnyedim <viktoronyedim@gmail.com>
2024-10-04 01:44:25 +0100

    Created pascal triangle function

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Function that returns a list of lists of integers 
+representing the Pascal's triangle of n
+"""
+
+
+def pascal_triangle(n):
+    """Returns a list of integers representing the Pascal's triangle of n"""
+
+    if n <= 0:
+        return []
+
+    triangle = []
+
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            # Set the first and last element in each row to be 1
+            if j == 0 or j == i:
+                row.append(1)
+            else:
+                # Sum the two numbers just above the number
+                row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        triangle.append(row)
+    return triangle
