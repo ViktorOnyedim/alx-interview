35c5310a9e2cdaa8fb4e018f763d8674d635e3cd
ViktorOnyedim <viktoronyedim@gmail.com>
2024-10-18 16:20:46 +0100

    implemented minOperations function

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+
+def minOperations(n):
+    """return minimum number of Copy-Paste Operations to reach target"""
+    if n <= 1:
+        return 0
+
+    operations = 0
+    current_no = 1
+    clipboard = 0
+
+    while current_no < n:
+        if n - current_no >= current_no:
+            # Copy all and Paste
+            clipboard = current_no
+            current_no += clipboard
+            operations += 2
+        else:
+            # Only Paste
+            current_no += clipboard
+            operations += 1
+
+    return operations
+
