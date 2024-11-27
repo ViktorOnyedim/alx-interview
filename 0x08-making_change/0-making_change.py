5ff370604837c9a9f418374b81dfb85fc109f924
ViktorOnyedim <viktoronyedim@gmail.com>
2024-11-27 15:58:02 +0100

    pycodestyle validation

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -22,31 +22,3 @@ def makeChange(coins, total):
             total -= num_coins * coin
 
     return count if total == 0 else -1
-
-    # dp = [float('inf')] * (total + 1)
-    # dp[0] = 0
-
-    # for i in range(1, total + 1):
-    #     for coin in coins:
-    #         if i - coin >= 0:
-    #             dp[i] = min(dp[i], dp[i - coin] + 1)
-
-    # return dp[total] if dp[total] != float('inf') else -1
-
-# def makeChange(coins, total):
-#     if total <= 0:
-#         return 0
-
-#     # Sort coins in descending order for the greedy approach
-#     coins.sort(reverse=True)
-#     count = 0
-
-#     for coin in coins:
-#         if total == 0:
-#             break
-#         if coin <= total:
-#             num_coins = total // coin
-#             count += num_coins
-#             total -= num_coins * coin
-
-#     return count if total == 0 else -1
