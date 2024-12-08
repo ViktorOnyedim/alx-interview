da08776eadfeac10d7c99ef15458edecdda50fa4
ViktorOnyedim <viktoronyedim@gmail.com>
2024-12-09 00:42:21 +0100

    implemented island perimeter function

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Island Perimeter Problem"""
+
+
+def island_perimeter(grid):
+    """Calculate the perimeter of the island described in grid"""
+    rows = len(grid)
+    cols = len(grid[0])
+    perimeter = 0
+
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                perimeter += 4
+
+            if row > 0 and grid[row - 1][col] == 1:
+                perimeter -= 1
+            if row < rows - 1 and grid[row + 1][col] == 1:
+                perimeter -= 1
+            if col > 0 and grid[row][col - 1] == 1:
+                perimeter -= 1
+            if col < cols - 1 and grid[row][col + 1] == 1:
+                perimeter -= 1
+    return perimeter
