c48bed53d1d9b11e9d3ddcc38e1d0334f82ae192
ViktorOnyedim <viktoronyedim@gmail.com>
2024-12-09 00:48:21 +0100

    Modified function

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -13,12 +13,12 @@ def island_perimeter(grid):
             if grid[row][col] == 1:
                 perimeter += 4
 
-            if row > 0 and grid[row - 1][col] == 1:
-                perimeter -= 1
-            if row < rows - 1 and grid[row + 1][col] == 1:
-                perimeter -= 1
-            if col > 0 and grid[row][col - 1] == 1:
-                perimeter -= 1
-            if col < cols - 1 and grid[row][col + 1] == 1:
-                perimeter -= 1
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 1
+                if row < rows - 1 and grid[row + 1][col] == 1:
+                    perimeter -= 1
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 1
+                if col < cols - 1 and grid[row][col + 1] == 1:
+                    perimeter -= 1
     return perimeter
