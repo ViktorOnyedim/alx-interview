84cc27208309156c92fbebbd249b911f3308d3cb
ViktorOnyedim <viktoronyedim@gmail.com>
2024-12-10 14:27:46 +0100

    solution to lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    n = len(boxes)
+
+    # Start with the first box that is unlocked
+    unlocked = set([0])
+    # Keys from the first box
+    keys = set(boxes[0])
+
+    while keys:
+        new_key = keys.pop()
+        if new_key < n and new_key not in unlocked:
+            unlocked.add(new_key)
+            keys.update(boxes[new_key])
+
+    return len(unlocked) == n
+
